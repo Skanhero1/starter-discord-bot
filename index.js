9d1f0270d71544a9a0a0c8a4c7ac0f149282323d
Skanhero1 <135775278+Skanhero1@users.noreply.github.com>
2023-06-08 03:18:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -102,9 +102,7 @@ app.get('/', async (req,res) =>{
 })
 
 app.post('/post', (request, response) => {
-    response.send("Gotten POST request")
-	console.log(request.something)
-	return "aa"
+    response.send({"something": 2})
 })
 
 app.listen(8999, () => {
