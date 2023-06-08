da1b093b335a8e4c147cf85e5730291cc23f40be
Skanhero1 <135775278+Skanhero1@users.noreply.github.com>
2023-06-08 05:45:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.post('/checkMemberV2', async (req, res) => {
 	let response = undefined
 	
 	try {
-		console.log(req.data.username)
+		console.log(req.body.username)
 		response = await discord_api.get(`/users`, {
   params: {
     username: req.body.username,
